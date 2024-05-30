3c9d3490173fb3915b8bae838ac9d827d365c366
Mike Biang <mike.biang@gmail.com>
2024-05-30 11:28:14 -0500

    Illumate 11 compatibility

--- Container.php
+++ Container.php
@@ -6,4 +6,18 @@ use Illuminate\Container\Container as BaseContainer;
 
 class Container extends BaseContainer
 {
+
+    protected $terminatingCallbacks = [];
+
+    public function terminating($callback)
+    {
+        $this->terminatingCallbacks[] = $callback;
+        return $this;
+    }
+    
+    public function terminate(){
+        foreach($this->termterminatingCallbacks as $callback){
+            $callback();
+        }       
+    }
 }
